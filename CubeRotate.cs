53009a66f2d8fcebac29dff5e982f36c8316b60a
jingle23 <wlsrms23@naver.com>
2016-03-17 12:44:16 +0900

    황금큐브찾기 큐브의 회전패턴 구현

---
+++ CubeRotate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeRotate : MonoBehaviour {
+	public bool isXAxis = false;
+	public int CurrentRotateIndex = 0;
+
+	public Vector3 m_XPoint;
+	public Vector3 m_YPoint;
+	
+	public void Rotate(){
+		StartCoroutine ("RotateCoroutine");
+	}
+
+	IEnumerator RotateCoroutine(){
+		yield return new WaitForSeconds(0.5f);
+
+		float fAngle = 0;
+		if (isXAxis) {
+			while(fAngle<360){
+				float frameAngle = Time.deltaTime * 200;
+				fAngle += frameAngle;
+				transform.RotateAround(m_XPoint,new Vector3(1,0,0), frameAngle);
+				yield return null;
+			}
+		} else {
+			while(fAngle<360){
+				float frameAngle = Time.deltaTime * 200;
+				fAngle += frameAngle;
+				transform.RotateAround(m_YPoint,new Vector3(0,1,0), frameAngle);
+				yield return null;
+			}
+		}
+
+		GameObject.Find ("Yaba").GetComponent<RotateManager> ().SetBool (CurrentRotateIndex);
+	}
+}
